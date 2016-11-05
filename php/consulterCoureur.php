9f7eddf557e3d151bc9bc292427d202ab8e25498
JulienChevron <chevron-j@hotmail.fr>
2016-11-05 11:44:40 +0100

    filtre coureur

--- php/consulterCoureur.php
+++ php/consulterCoureur.php
@@ -26,12 +26,15 @@
     ?>
 
        <form method="get" name="form" id="form" action="<?=$_SERVER['PHP_SELF']?>">
+        <fieldset><legend>Fitlre</legend>
         <label class="form_col" for="nom">Nom :</label>
-        <input type="text" name="nom" id="nom"/></br>
+        <input type="text" name="nom" id="nom"/>
         <label class="form_col" for="prenom">Prénom :</label>
-        <input type="text" name="prenom" id="prenom"/></br>
+        <input type="text" name="prenom" id="prenom"/>
+        <label class="form_col" for="annee_naiss">Année de naissance :</label>
+        <input type="number" name="annee_naiss" id="annee_naiss" min="1900" max="2016"/></br>
         <label class="form_col" for="annee_prem">Année de premier tour :</label>
-        <input type="number" name="annee_prem" id="annee_prem" min="1900" max="2016"/></br>
+        <input type="number" name="annee_prem" id="annee_prem" min="1900" max="2016"/>
         <label class="form_col" for="code_tdf">Code tdf :</label>
             <select name="code_tdf" id="code_tdf">
                 <?php 
@@ -40,6 +43,7 @@
             </select>
         </br>
         <input type="submit" name="Envoyer" id="Envoyer"/>
+        </fieldset>
     </form>
 
     <?php
@@ -49,6 +53,7 @@
             else if(strcmp($_GET['supp'],'erreur')==0)
                 echo "Suppression impossible, le coureur a déjà participé au tour";
         }
+        include('formatage.php');
         include('consulterCoureurFonc.php');
         afficherCoureur($conn);
     ?>
--- php/consulterCoureurFonc.php
+++ php/consulterCoureurFonc.php
@@ -2,43 +2,73 @@
 
 function afficherCoureur($bdd){
 
-        if(isset($_GET["nom"]) && isset($_GET["prenom"]) && isset($_GET["annee_prem"]) && isset($_GET["code_tdf"])){
-
-            $sql = "SELECT * FROM TDF_COUREUR WHERE ANNEE_PREM = '" . $_GET["annee_prem"] . "'AND CODE_TDF = '" . $_GET["code_tdf"] . "'  ORDER BY N_COUREUR DESC";
-            $reponse = $bdd->query($sql);
-        
-             echo '<table class="table table-condensed" id="table"><thead> <tr> <th>Numero</th> <th>Prenom</th> <th>Nom</th> <th>Code TDF</th> <th>Année de naissance</th><th>Année premier tour</th>';
-            while ($donnees = $reponse->fetch())
-            {   
-                $num = $donnees['N_COUREUR'];
-                $codetdf = $donnees['CODE_TDF'];
-                $nom = $donnees['NOM'];
-                $prenom = $donnees['PRENOM'];
-                $naiss = $donnees['ANNEE_NAISSANCE'];
-                $anneetdf = $donnees['ANNEE_PREM'];
-                echo '<tr id="ligne"><td>' . $num . '</td><td>' . $prenom . '</td><td>' . $nom . '</td><td>' . $codetdf . '</td><td>' . $naiss . '</td><td>'. $anneetdf . '</td><td><a href="supprimerCoureur.php?ncoureur=' . $num . '&nom=' . $nom . '&prenom=' . $prenom . '">Supprimer</a></td></td><td><a href="modifierCoureur.php?ncoureur=' . $num . '&nom=' . $nom . '&prenom=' . $prenom . '&naiss=' . $naiss . '&anneetdf=' . $anneetdf . '&codetdf=' . $codetdf . '">Modifier</a></td></tr>';
-            }
-            echo '</table>';
-            $reponse->closeCursor();
+        if(isset($_GET["nom"]) && isset($_GET["prenom"]) && isset($_GET["annee_prem"]) && isset($_GET["annee_naiss"]) && isset($_GET["code_tdf"])){
+            echo "FILTRE0";
+            $sql = requeteFiltre($_GET["nom"],$_GET["prenom"], $_GET["annee_prem"], $_GET["annee_naiss"], $_GET["code_tdf"]);
+            echo "FILTRE1";
+            Afficher($sql);
         }else{
+            $sql = 'SELECT * FROM TDF_COUREUR ORDER BY N_COUREUR DESC';
+        }
 
-        $sql = 'SELECT * FROM TDF_COUREUR ORDER BY N_COUREUR DESC';
         $reponse = $bdd->query($sql);
        
         echo '<table class="table table-condensed" id="table"><thead> <tr> <th>Numero</th> <th>Prenom</th> <th>Nom</th> <th>Code TDF</th> <th>Année de naissance</th><th>Année premier tour</th>';
         while ($donnees = $reponse->fetch())
         {   
-            $num = $donnees['N_COUREUR'];
-            $codetdf = $donnees['CODE_TDF'];
-            $nom = $donnees['NOM'];
-            $prenom = $donnees['PRENOM'];
-            $naiss = $donnees['ANNEE_NAISSANCE'];
-            $anneetdf = $donnees['ANNEE_PREM'];
+            $num = utf8_encode($donnees['N_COUREUR']);
+            $codetdf = utf8_encode($donnees['CODE_TDF']);
+            $nom = utf8_encode($donnees['NOM']);
+            $prenom = utf8_encode($donnees['PRENOM']);
+            $naiss = utf8_encode($donnees['ANNEE_NAISSANCE']);
+            $anneetdf = utf8_encode($donnees['ANNEE_PREM']);
             echo '<tr id="ligne"><td>' . $num . '</td><td>' . $prenom . '</td><td>' . $nom . '</td><td>' . $codetdf . '</td><td>' . $naiss . '</td><td>'. $anneetdf . '</td><td><a href="supprimerCoureur.php?ncoureur=' . $num . '&nom=' . $nom . '&prenom=' . $prenom . '">Supprimer</a></td></td><td><a href="modifierCoureur.php?ncoureur=' . $num . '&nom=' . $nom . '&prenom=' . $prenom . '&naiss=' . $naiss . '&anneetdf=' . $anneetdf . '&codetdf=' . $codetdf . '">Modifier</a></td></tr>';
         }
         echo '</table>';
         $reponse->closeCursor();
-        }
+        
     }
+
+function requeteFiltre($nom,$prenom,$prem,$naiss,$tdf){
+    $cpt = 0;
+    $sql = "SELECT * FROM TDF_COUREUR ";
+    if(strcmp($nom, "") != 0){
+        $nom = formaterNom($nom);
+        $sql = $sql . "WHERE NOM LIKE '%" . $nom ."%' ";
+        $cpt++;
+    }
+    if(strcmp($prenom, "") != 0){
+        $prenom = formaterPrenom($prenom);
+        if($cpt ==0)
+            $sql = $sql . "WHERE PRENOM LIKE '%" . $prenom ."%' ";
+        else
+            $sql = $sql . "AND PRENOM LIKE '%" . $prenom ."%' ";
+        $cpt++;
+    }
+    if(strcmp($prem, "") != 0){
+        if($cpt ==0)
+            $sql = $sql . "WHERE ANNEE_PREM = '" . $prem ."' ";
+        else
+            $sql = $sql . "AND ANNEE_PREM = '" . $prem ."' ";
+        $cpt++;
+    }
+     if(strcmp($naiss, "") != 0){
+        if($cpt ==0)
+            $sql = $sql . "WHERE ANNEE_NAISS = '" . $naiss ."' ";
+        else
+            $sql = $sql . "AND ANNEE_NAISS = '" . $naiss ."' ";
+        $cpt++;
+    }
+    if(strcmp($tdf, "") != 0){
+        if($cpt ==0)
+            $sql = $sql . "WHERE CODE_TDF = '" . $tdf ."' ";
+        else
+            $sql = $sql . "AND CODE_TDF = '" . $tdf ."' ";
+        $cpt++;
+    }
+    $sql = $sql . "ORDER BY N_COUREUR DESC";
+    return $sql;
+
+}
 
 ?>
