3e66f2d3423694e4b2374e7f81296ce61102d9b5
JulienChevron <chevron-j@hotmail.fr>
2016-11-05 22:05:51 +0100

    Jamais assez de commit

--- php/coureurPlusFonc.php
+++ php/coureurPlusFonc.php
@@ -7,13 +7,12 @@
         $anneeMax = $donnees['ANNEE_MAX'];
         $reponse->closeCursor();
         echo '<table class="table table-condensed" id="table"><thead> <tr> <th>ANNEE</th> <th>CLASSEMENT</th> <th>TEMPS TOTAL</th></thead>';
-        for($i = $anneeMax; $i>=$prem;$i--){ //$i = $prem; $i<=$anneeMax;$i++
+        for($i = $anneeMax; $i>=$prem;$i--){
             try{
                 $sqlV = "SELECT N_COUREUR, ANNEE FROM TDF_PARTI_COUREUR WHERE N_COUREUR = " . $ncoureur . " AND ANNEE = " .$i;
                 $reponse = $bdd->query($sqlV);
                 $valeur = $reponse->fetchAll();
                 if (count($valeur) != 0){
-                    echo $i;
                     $sql = "SELECT * FROM TDF_CLASSEMENT_" . $i . " WHERE N_COUREUR = " . $ncoureur;
                     $reponse = $bdd->query($sql);
                     while ($donnees = $reponse->fetch())
@@ -32,7 +31,26 @@
         echo '</table>';
     }
 
-    function afficherEquipes($conn,$ncoureur){
-        echo "OUI";
+    function afficherEquipes($bdd,$ncoureur){
+        echo '<table class="table table-condensed" id="table"><thead> <tr> <th>ANNEE</th> <th>EQUIPE</th></thead>';
+        try{
+                $sqlV = "SELECT N_COUREUR FROM TDF_PARTI_COUREUR WHERE N_COUREUR = " . $ncoureur;
+                $reponse = $bdd->query($sqlV);
+                $valeur = $reponse->fetchAll();
+                if (count($valeur) != 0){
+                    $sql = "SELECT ANNEE, N_COUREUR, N_SPONSOR, NOM FROM TDF_PARTI_COUREUR JOIN TDF_SPONSOR USING (N_SPONSOR,N_EQUIPE) WHERE N_COUREUR = " .$ncoureur . " ORDER BY ANNEE DESC";
+                    $reponse = $bdd->query($sql);
+                    while ($donnees = $reponse->fetch())
+                    {   
+                        $annee = $donnees['ANNEE'];
+                        $equipe = $donnees['NOM'];
+                        echo '<tr id="ligne"><td>' . $annee . '</td><td>' . $equipe . '</td></tr>';
+                    }
+                    $reponse->closeCursor();
+               }
+            }catch(PDOException $e){
+
+            }
+        echo '</table>';
     }
 ?>
--- php/supprimerCoureur.php
+++ php/supprimerCoureur.php
@@ -9,7 +9,8 @@
 		$prenom = $_GET['prenom'];
 		$sql1 = "SELECT N_COUREUR FROM TDF_PARTI_COUREUR WHERE N_COUREUR = " . $ncoureur;
 		$reponse = $conn->query($sql1);
-        if($reponse->rowCount() == 0){
+        $valeur = $reponse->fetchAll();
+        if (count($valeur) == 0){
             $sql = 'DELETE FROM TDF_COUREUR WHERE N_COUREUR = ' . $ncoureur;
 			$conn->exec($sql);
 	        header('Location: consulterCoureur.php?supp=ok');
